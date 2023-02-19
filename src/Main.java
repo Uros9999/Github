685c8c68eec60a6e9c6767f0274eb692f99f51e2
unknown <skocajicg@gmail.com>
2023-02-19 12:50:42 +0100

    changes

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        //github-server
+
+
+
+
+        //git --version v git -version      -  da proverimo da li nam je instaliran github i koja je verzija
+
+
+
+        // git init              -      sa ovom komandom povezujemo git sa lokala sa github-om
+
+
+
+        //git branch          - sa ovim pravim novu granu,osnovna je main(ovo je recimo add new changes)
+
+
+
+        //git add .      -dodavanje promena
+
+
+        //git commit -m "adding new changes regarding main method"       -ovime smo spremili na fajl sa promenama i spreman je da ode na server
+
+
+        //git push origin      - pusovanje na github
+
+
+        //git push origin/add new changes      -   primer kad saljemo neke promene, ide / pa naziv
+
+
+
+
+
+
+
+    }
+
+}
